4cca0cefbf056ae9696293585a72338bcc01b2a3
black-tomcat-dev <kevin.vdm@protonmail.com>
2018-02-16 12:15:34 +0000

    testing

--- index.js
+++ index.js
@@ -2,11 +2,9 @@ var cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
-console.log("test1" , process.env.TIMES);
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').load();
 }
-console.log('test2', process.env.TIMES);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -15,11 +13,11 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (request, response) => response.send(cool()))
   .get('/times', function(request, response){
-    var result = '';
-    var times = process.env.TIMES || 5;
-    
-    for (var i=0; i < times; i++)
-      result += i + ' ';
-      response.send(result);
+    var id = process.env.MONGOLAB_URI
+    // var result = '';
+    // var times = process.env.TIMES || 5;    
+    // for (i=0; i < times; i++)
+    //   result += i + ' ';
+      response.send(id);
     
   }).listen(PORT, () => console.log(`Listening on ${ PORT }`))
