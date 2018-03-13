b2adf913592648184548832c990a574115f01a6c
black-tomcat-dev <kevin.vdm@protonmail.com>
2018-03-13 17:29:26 +0000

    sockets.io

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ var app = express();
 /** socket io */
 users = {};
 // const io = new WebSocketServer({port: 40510 });
-// var server=require('http').createServer(app);
-// var io =require('socket.io').listen(server);
+var server=require('http').createServer(app);
+var io =require('socket.io').listen(server);
 // const io = socketIO(server);
 // io.configure(function () { 
 //   io.set("transports", ["xhr-polling"]); 
@@ -20,7 +20,7 @@ users = {};
 // });
 // socket = new io.Socket();
 
-  app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+  server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 // app.listen(8080 || PORT);
 
 
@@ -118,65 +118,65 @@ var routes=require('./routes/route.js');
   });
 
 
-  // io.on('connection',function(socket){
+  io.sockets.on('connection',function(socket){
 
-  //   console.log("A New Connection Established");
+    console.log("A New Connection Established");
 
-  //   socket.on('new user',function(data,callback){
-  //     if(data in users){
-  //       // console.log("Username already taken");
-  //       callback(false);
-  //     }else{
-  //       // console.log("Username available");
-  //       callback(true);
-  //       socket.nickname=data;
-  //       users[socket.nickname]=socket;
-  //       updateNicknames();
-  //     }
-  //   });
-
-
-  //   function updateNicknames(){
-  //     io.emit('usernames',Object.keys(users));
-  //   }
+    socket.on('new user',function(data,callback){
+      if(data in users){
+        console.log("Username already taken");
+        callback(false);
+      }else{
+        console.log("Username available");
+        callback(true);
+        socket.nickname=data;
+        users[socket.nickname]=socket;
+        updateNicknames();
+      }
+    });
 
 
-  //   socket.on('send message',function(data,callback){
-  //     var msg=data.trim();
-
-  //     // if(msg.substr(0,1) === '@'){
-  //     //   msg=msg.substr(1);
-  //     //   var ind=msg.indexOf(' ');
-  //     //   if(ind !== -1){
-  //     //     var name=msg.substring(0,ind);
-  //     //     var msg=msg.substring(ind+1);
-  //     //      if(name in users){
-  //     //         users[name].emit('whisper',{msg:msg,nick:socket.nickname});
-  //     //         socket.emit('private',{msg:msg,nick:name});
-  //     //       console.log("Whispering !");
-  //     //     }else{
-  //     //       callback("Sorry, "+name+" is not online");
-  //     //     }
-  //     //   }else{
-  //     //     callback("Looks like you forgot to write the message");
-  //     //   }
-
-  //     // }
-
-  //     //  else{
-  //      console.log("Got Message :"+data)
-  //     io.emit('new message',{msg:msg,nick:socket.nickname});
-  //       //  }
-  //   });
+    function updateNicknames(){
+      io.sockets.emit('usernames',Object.keys(users));
+    }
 
 
-  //   socket.on('disconnect',function(data){
-  //         if(!socket.nickname) return;
-  //         delete users[socket.nickname];
-  //         updateNicknames();
-  //   });
+    socket.on('send message',function(data,callback){
+      var msg=data.trim();
+
+      // if(msg.substr(0,1) === '@'){
+      //   msg=msg.substr(1);
+      //   var ind=msg.indexOf(' ');
+      //   if(ind !== -1){
+      //     var name=msg.substring(0,ind);
+      //     var msg=msg.substring(ind+1);
+      //      if(name in users){
+      //         users[name].emit('whisper',{msg:msg,nick:socket.nickname});
+      //         socket.emit('private',{msg:msg,nick:name});
+      //       console.log("Whispering !");
+      //     }else{
+      //       callback("Sorry, "+name+" is not online");
+      //     }
+      //   }else{
+      //     callback("Looks like you forgot to write the message");
+      //   }
+
+      // }
+
+      //  else{
+       console.log("Got Message :"+data)
+      io.sockets.emit('new message',{msg:msg,nick:socket.nickname});
+        //  }
+    });
 
 
-// });
+    socket.on('disconnect',function(data){
+          if(!socket.nickname) return;
+          delete users[socket.nickname];
+          updateNicknames();
+    });
+
+
+});
 
 
